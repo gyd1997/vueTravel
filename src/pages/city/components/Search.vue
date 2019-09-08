e0f4fd07a62d1d355dab0e916576c909e10b9da9
gyd1997 <gyd0008@163.com>
2019-09-08 21:01:29 +0800

    use vuex

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -8,6 +8,7 @@
         <li class="search-item border-bottom"
             v-for="item of list"
             :key="item.id"
+            @click="handleCityClick(item.name)"
         >{{item.name}}</li>
         <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
       </ul>
@@ -17,6 +18,7 @@
 
 <script>
 import Bscroll from 'better-scroll'
+import { mapMutations } from 'vuex'
 export default {
   name: 'CitySearch',
   data () {
@@ -58,6 +60,13 @@ export default {
     hasNoData () {
       return !this.list.length
     }
+  },
+  methods: {
+    handleCityClick (city) {
+      this.changeCity(city)
+      this.$router.push('/')
+    },
+    ...mapMutations(['changeCity'])
   }
 }
 </script>
